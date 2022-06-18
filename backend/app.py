bb68d46342ac2f7c3c21cf25c6607996780bf030
philippschalau <philipp.schalau@hotmail.de>
2022-06-18 12:53:08 +0200

    server works now with emulator

--- backend/app.py
+++ backend/app.py
@@ -1,31 +1,30 @@
 import os
+from keras import backend as k
+import tensorflow as tf
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image as img
 from tensorflow.keras.utils import img_to_array
-from keras import backend as k
+from tensorflow.keras.applications.resnet50 import ResNet50, preprocess_input
 import numpy as np
-import tensorflow as tf
+from flask import Flask, request, jsonify, render_template
 from PIL import Image
-from datetime import datetime
-from tensorflow.keras.applications.resnet50 import ResNet50, decode_predictions, preprocess_input
-from flask import Flask, Blueprint, request, render_template, jsonify
 
 model = load_model("resNet50_16_batches_best.h5")
 
 app = Flask(__name__)
 
 
-@app.route('/uploades', methods=["GET", "POST"])
+@app.route('/', methods=["POST"])
 def upload():
     if (request.method == "POST"):
         imagefile = request.files['file']
-        path = os.path.join(os.getcwd() + '\\uploades\\' + imagefile.filename)
+        path = os.path.join(os.getcwd() +"/"+ imagefile.filename)
         imagefile.save(path)
         prediction = identifyImage(path)
         os.remove(path)
         return jsonify({
-                        "prediction": prediction
-                        })
+            "prediction": prediction
+        })
 
 
 def identifyImage(img_path):
@@ -35,7 +34,7 @@ def identifyImage(img_path):
     x = np.expand_dims(x, axis=0)
     x = preprocess_input(x)
     preds = model.predict(x)
-    predicted_percentage = round(100 * np.max(preds),2)
+    predicted_percentage = round(100 * np.max(preds), 2)
     predicted_tree_name = class_names[np.argmax(preds[0], axis=-1)]
     return {
         "predicted tree": predicted_tree_name,
@@ -43,5 +42,12 @@ def identifyImage(img_path):
     }
 
 
+# @app.route('/')
+# def index():
+#     # A welcome message to test our server
+#     return "<h1>Welcome to our medium-greeting-api Test!</h1>"
+
+port = int(os.environ.get('PORT', 5000))
+
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(host='0.0.0.0', port=port, debug=True)
